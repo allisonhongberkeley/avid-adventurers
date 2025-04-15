c5b981c9a7bda7b3d632d56a0fcc40e640763d88
Allison Hong <allisonhong1023@berkeley.edu>
2025-04-15 00:36:07 -0700

    [feat] create Back Button

--- avid-adventurers/src/pages/onboarding/Age.tsx
+++ avid-adventurers/src/pages/onboarding/Age.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ProfileImage} from '../../components/ProfileImage/ProfileImage';
 import { LabeledInput } from '../../components/LabeledInput/LabeledInput';
-import { Container, Title, FormWrapper, ProfileImageWrapper, ProfileImageInput, PlaceholderText, ProfileImagePreview, FallbackText, Button } from '../styles';
+import { Container, Title, FormWrapper, ButtonRow, BackButton, ContinueButton } from '../styles';
 import { useOnboarding } from '../../utils/onboardingContext';
 
 
@@ -10,10 +10,13 @@ const Age: React.FC = () => {
     const navigate = useNavigate();
     const { age, setAge, pronouns, setPronouns, funFact, setFunFact, profileImage, setProfileImage } = useOnboarding();
 
-
     const handleContinue = () => {
       navigate('/onboarding/age');
     };
+
+    const handleBack = () => {
+      navigate('/onboarding/profile');
+    }
 
     return (
     <Container>
@@ -43,8 +46,10 @@ const Age: React.FC = () => {
           value={funFact}
           onChange={(e) => setFunFact(e.target.value)}
         />
-
-        <Button onClick={handleContinue}>Continue</Button>
+        <ButtonRow>
+          <BackButton onClick={handleBack}>Back</BackButton>
+          <ContinueButton onClick={handleContinue}>Continue</ContinueButton>
+        </ButtonRow>
       </FormWrapper>
     </Container>
 )};
--- avid-adventurers/src/pages/onboarding/Profile.tsx
+++ avid-adventurers/src/pages/onboarding/Profile.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ProfileImage} from '../../components/ProfileImage/ProfileImage';
 import { LabeledInput } from '../../components/LabeledInput/LabeledInput';
-import { Container, Title, FormWrapper, ProfileImageWrapper, ProfileImageInput, PlaceholderText, ProfileImagePreview, FallbackText, Button } from '../styles';
+import { Container, Title, FormWrapper, ButtonRow, BackButton, ContinueButton } from '../styles';
 import { useOnboarding } from '../../utils/onboardingContext';
 
 const Profile: React.FC = () => {
@@ -12,6 +12,10 @@ const Profile: React.FC = () => {
     const handleContinue = () => {
       navigate('/onboarding/age');
     };
+
+    const handleBack = () => {
+      navigate('/');
+    }
 
     return (
     <Container>
@@ -35,7 +39,10 @@ const Profile: React.FC = () => {
         onChange={(e) => setCity(e.target.value)}
       />
 
-      <Button onClick={handleContinue}>Continue</Button>
+      <ButtonRow>
+        <BackButton onClick={handleBack}>Back</BackButton>
+        <ContinueButton onClick={handleContinue}>Continue</ContinueButton>
+      </ButtonRow>
       </FormWrapper>
     </Container>
 )};
