dcc9f859095293be068dcf58b831484c16f3be7b
Jeremy Saputo <Jeremy@Jeremys-MacBook-Air.local>
2025-05-07 10:55:42 -0700

    Edited confetti width back to phone screen size

--- avid-adventurers/src/pages/styles.ts
+++ avid-adventurers/src/pages/styles.ts
@@ -149,7 +149,7 @@ export const BackgroundWrapper = styled.div`
 `;
 
 export const PhoneContainer = styled.div`
-  width: 100%;
+  width: 400px;
   min-height: 100vh;
   background-image: url('https://cdn.pixabay.com/animation/2024/05/02/07/43/07-43-00-535_512.gif');
   background-size: cover;
